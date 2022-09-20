296aed35f850f21d1370f2c4314fdddc12f777b8
duvancho303 <duvancho303@gmail.com>
2022-09-19 20:38:45 -0600

    adding more code

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
-/* variables */
+// Variables
+const form = document.getElementById('#send-email')
 
+
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* variables */
+/*
 const btnSend = document.querySelector('#send');
 
 // Variables for input - email, subject and message
@@ -35,3 +65,4 @@ function validateForm(b) {
     }
 }
 
+*/
