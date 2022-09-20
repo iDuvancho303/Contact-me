19d33d0098fc87655ef7ec9a1ba7030b7ff9d72e
duvancho303 <duvancho303@gmail.com>
2022-09-19 21:38:54 -0600

    adding more code

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const expresiones = {
 	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{4,12}$/, // 4 a 12 digitos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
 
 }
@@ -15,7 +15,9 @@ const expresiones = {
 const validateForm = (e) => {
     switch (e.target.name) {
         case 'email':
-            console.log('funciona')
+            if(expresiones.email.test()){
+
+            }
         break;
         case 'subject':
             console.log('funciona2')
